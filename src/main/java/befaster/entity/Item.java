9b84da352a2255929b636fd2b2dc9c452f0703b3
sbx_user1051 <sbx_user1051@169.254.83.189>
2024-01-06 23:35:17 +0000

    Sat Jan 06 23:35:17 UTC 2024

--- src/main/java/befaster/entity/Item.java
+++ src/main/java/befaster/entity/Item.java
@@ -1,2 +1,26 @@
-package befaster.entity;public class Item {
+package befaster.entity;
+
+public class Item {
+    private Character sku;
+    private Integer price;
+    private SpecialOffer specialOffer;
+
+    public Item(Character sku, Integer price, SpecialOffer specialOffer) {
+        this.sku = sku;
+        this.price = price;
+        this.specialOffer = specialOffer;
+    }
+
+    public Character getSku() {
+        return sku;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public SpecialOffer getSpecialOffer() {
+        return specialOffer;
+    }
 }
+
--- src/main/java/befaster/entity/SpecialOffer.java
+++ src/main/java/befaster/entity/SpecialOffer.java
@@ -1,2 +1,25 @@
-package befaster.entity;public class SpecialOffer {
+package befaster.entity;
+
+public class SpecialOffer {
+    private Integer quantity;
+    private Integer fixedPrice;
+
+    public SpecialOffer(Character sku, Integer quantity, Integer fixedPrice) {
+        this.sku = sku;
+        this.quantity = quantity;
+        this.fixedPrice = fixedPrice;
+    }
+
+    public Character getSku() {
+        return sku;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Integer getFixedPrice() {
+        return fixedPrice;
+    }
 }
+
