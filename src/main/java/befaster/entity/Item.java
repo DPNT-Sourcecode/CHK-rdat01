96f1938e88b599ae0b9be2b4ab1106d6749e5054
sbx_user1051 <sbx_user1051@169.254.83.189>
2024-01-06 23:17:16 +0000

    Sat Jan 06 23:17:16 UTC 2024

--- src/main/java/befaster/entity/Item.java
+++ src/main/java/befaster/entity/Item.java
@@ -1,4 +1,11 @@
 package befaster.entity;
 
 public class Item {
+    private String sku;
+    private Integer price;
+
+    public Item(String sku, Integer price) {
+        this.sku = sku;
+        this.price = price;
+    }
 }
