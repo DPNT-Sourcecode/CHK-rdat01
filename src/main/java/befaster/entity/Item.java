cad611b93770735678990470e7445576deb7ea9d
sbx_user1051 <sbx_user1051@169.254.52.157>
2024-01-08 22:40:29 +0000

    Mon Jan 08 22:40:29 UTC 2024

--- src/main/java/befaster/entity/Item.java
+++ src/main/java/befaster/entity/Item.java
@@ -64,7 +64,7 @@ public class Item {
             SpecialOffer specialOffer,
             int freeItemBasketQuantity,
             HashMap<Character, Integer> basket) {
-        return getFinalPrice(basket.get(specialOffer.getFreeItemSKU()), basket) + freeItemBasketQuantity * basket;
+        return getFinalPrice(basket.get(specialOffer.getFreeItemSKU()), basket) + freeItemBasketQuantity * 30;
     }
 
     private int calculateSpecialPriceOffer(
@@ -80,6 +80,7 @@ public class Item {
         return remainderPrice + divisionResult * specialOffer.getPrice();
     }
 }
+
 
 
 
