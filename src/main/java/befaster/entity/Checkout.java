b81e25bb786b45639a304aa9ac4f8db41fd0c524
sbx_user1051 <sbx_user1051@169.254.37.229>
2024-01-09 20:54:47 +0000

    Tue Jan 09 20:54:47 UTC 2024

--- src/main/java/befaster/entity/Checkout.java
+++ src/main/java/befaster/entity/Checkout.java
@@ -57,10 +57,6 @@ public class Checkout {
             }
 
             if(specialOffer.isSameItemFreeOffer() && quantity % specialOffer.getQuantity() == 0){
-                return finalPrice - (quantity / 3) * specialOffer.getPrice();
-            }
-
-            if(specialOffer.isSameItemFreeOffer()){
                 return finalPrice - (quantity / (specialOffer.getQuantity()+1)) * specialOffer.getPrice();
             }
 
