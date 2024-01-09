83ce02cb948a54c6c7e0bcde7b22a6b0224961dd
sbx_user1051 <sbx_user1051@169.254.52.157>
2024-01-09 01:40:46 +0000

    Tue Jan 09 01:40:46 UTC 2024

--- src/main/java/befaster/entity/Checkout.java
+++ src/main/java/befaster/entity/Checkout.java
@@ -18,22 +18,18 @@ public class Checkout {
     public int calculateCheckoutValue() {
         int checkoutValue = 0;
         for (var basketEntry : basket.entrySet()) {
-            checkoutValue += getItemPrice(basketEntry.getKey(), basketEntry.getValue());
+            checkoutValue += calculateItemPrice(basketEntry.getKey(), basketEntry.getValue());
         }
         return checkoutValue;
     }
 
-    private int getItemPrice(Item item, int quantity) {
-        var freeItemQuantity = freeItems.getOrDefault(item.getSku(), 0);
-        if(freeItemQuantity > 0){
-            quantity = freeItemQuantity >= quantity ? 0 : quantity - freeItemQuantity;
-            freeItems.remove(item.getSku());
-        }
-
+    private int calculateItemPrice(Item item, int quantity) {
         int finalPrice = quantity * item.getPrice();
 
         if(!item.isSpecialOfferApplicable(quantity))
             return finalPrice;
+
+        var freeItemQuantity = freeItems.getOrDefault(item.getSku(), 0);
 
         for (var specialOffer : item.filterApplicableSpecialOffers(quantity)) {
             if(specialOffer.isFreeItemOffer()){
@@ -51,11 +47,12 @@ public class Checkout {
 
     private int calculateSpecialPriceOffer(Item item, int quantity, SpecialOffer specialOffer) {
         int remainder = quantity % specialOffer.getQuantity();
-        var remainderPrice = getItemPrice(item, remainder);
+        var remainderPrice = calculateItemPrice(item, remainder);
 
         int divisionResult = quantity / specialOffer.getQuantity();
 
         return remainderPrice + divisionResult * specialOffer.getPrice();
     }
 }
+
 
