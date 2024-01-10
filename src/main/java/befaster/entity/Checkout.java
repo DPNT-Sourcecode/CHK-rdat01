f4f0ae711aa101ec64b5266d94edd21136129504
sbx_user1051 <sbx_user1051@169.254.51.149>
2024-01-10 13:15:22 +0000

    Wed Jan 10 13:15:22 UTC 2024

--- src/main/java/befaster/entity/Checkout.java
+++ src/main/java/befaster/entity/Checkout.java
@@ -13,15 +13,11 @@ public class Checkout {
             return (valueCompare != 0) ? valueCompare : item1.getSku().compareTo(item2.getSku());
         });
 
-        this.itemsInGroupDiscount = new TreeMap<>(
-                new Comparator<Item>() {
-                    @Override
-                    public int compare(Item item1, Item item2) {
-                        var valueCompare = Integer.compare(item1.getPrice(), item2.getPrice());
-                        return valueCompare != 0 ? valueCompare : 1;
-                    }
-                });
-                
+        this.itemsInGroupDiscount = new TreeMap<>((item1, item2) -> {
+            var valueCompare = Integer.compare(item1.getPrice(), item2.getPrice());
+            return valueCompare != 0 ? valueCompare : 1;
+        });
+
         this.freeItems = new HashMap<>();
     }
 
@@ -152,3 +148,4 @@ public class Checkout {
         return checkoutGroupDiscountValue;
     }
 }
+
